472d24459fa1d82d254ac1840b2c607c1dd6204e
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-01-02 16:32:10 +0100

    dsu

--- GoodBye2022/D/main.cpp
+++ GoodBye2022/D/main.cpp
@@ -30,8 +30,31 @@ typedef vector<pair<long long, long long>> vpll;
 #define sz(x) (ll)(x).size()
 #define nl cout << '\n'
 
+constexpr ll MOD = 998244353;
+
 ll a[100007], b[100007];
 set<ll> ust;
+ll par[100007], roz[100007];
+ll wiel1[100007], wiel2[100007];
+bitset<100007> jed;
+
+int f(int a)
+{
+    if(par[a] == a) return a;
+    return (par[a] = f(par[a]));
+}
+
+void u(int a, int b)
+{
+    a = f(a);
+    b = f(b);
+    if(a != b)
+    {
+        if(roz[a] > roz[b]) swap(a, b);
+        par[a] = b;
+        roz[b] += roz[a];
+    }
+}
 
 int main()
 {
@@ -44,24 +67,58 @@ int main()
     {
         ll n;
         cin >> n;
-        for(ll i = 0; i < n; i++)
-        {
+        rep(i, 1, n)
             cin >> a[i];
-        }
-        for(ll i = 0; i < n; i++)
-        {
+        rep(i, 1, n)
             cin >> b[i];
-        }
 
-        ll perm = 1;
-        for(ll i = 0; i < n; i++)
+        rep(i, 1, n)
         {
-            if(ust.count(a[i]) || ust.count(b[i])) continue;
-
+            par[i] = i;
+            roz[i] = 1;
         }
+        rep(i, 1, n)
+            u(a[i], b[i]);
+
+        rep(i, 1, n)
+        {
+            wiel1[i] = 0;
+            wiel2[i] = 0;
+        }
+        rep(i, 1, n)
+        {
+            wiel1[f(a[i])]++;
+            wiel2[f(i)]++;
+        }
+        bool ok = 1;
+        jed.reset();
+        rep(i, 1, n)
+        {
+            debug(i, wiel1[i], wiel2[i]);
+            if(wiel1[f(i)] != wiel2[f(i)]) ok = 0;
+            if(a[i] == b[i]) jed[f(a[i])] = 1;
+        }
+        if(!ok)
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        ll res = 1;
+        rep(i, 1, n)
+        {
+            if(i != par[i]) continue;
+            if(jed[i]) res = (res * n) % MOD;
+            else res = (res * 2) % MOD;
+        }
+
+        cout << res << '\n';
     }
 }
 
 /*
-
+1
+3
+1 2 2
+1 3 3
 */
